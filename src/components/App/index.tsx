d444a35fbe12e91aab7028354a398f0c9e35ecdd
JSvadim <sadomazo323@gmail.com>
2022-05-11 14:15:36 +0300

    Added useEffect for Auth check

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,3 +1,6 @@
+// hooks
+import { useEffect } from 'react';
+
 // third-party
 import { Routes, Route } from 'react-router-dom';
 
@@ -10,21 +13,33 @@ import MyBlogsPage from '../../pages/MyBlogs';
 import BlogsPage from '../../pages/Blogs';
 import BlogPage from '../../pages/Blog';
 import NotFoundPage from '../../pages/NotFound';
+import { AuthController } from '../../controllers/auth-controller';
+import { localStorageAccessToken } from '../../constants/local-storage';
+
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const checkAuth = async () => {
+      await AuthController.refresh()
+    }
+
+    if(localStorage.getItem(localStorageAccessToken)) {
+      checkAuth();
+    }
+  } , [])
 
   return (
     <>
       <Header/>
-      <Routes>
-        <Route path="/" element={ <BlogsPage/> }/>
-        <Route path="/blog" element={ <BlogPage/> }/>
-        <Route path="/my-blogs" element={ <MyBlogsPage/> }/>
-        <Route path="/add-blog" element={ <AddBlogPage/> }/>
-        <Route path="/log-in" element={ <LogInPage/> }/>
-        <Route path="/sign-in" element={ <SignInPage/> }/>
-        <Route path="*" element={ <NotFoundPage/> }/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={ <BlogsPage/> }/>
+          <Route path="/blog" element={ <BlogPage/> }/>
+          <Route path="/my-blogs" element={ <MyBlogsPage/> }/>
+          <Route path="/add-blog" element={ <AddBlogPage/> }/>
+          <Route path="/log-in" element={ <LogInPage/> }/>
+          <Route path="/sign-in" element={ <SignInPage/> }/>
+          <Route path="*" element={ <NotFoundPage/> }/>
+        </Routes>
     </>
   );
 
