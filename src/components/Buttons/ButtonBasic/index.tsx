a0a4696f51fb240d0af7e470ff2e1c87f72c318c
JSvadim <sadomazo323@gmail.com>
2022-12-23 19:33:18 +0300

    Fixed "ButtonBasic" hover on mobile devices
    Hover on mobile devices looked ugly.
    At first I tried to solve it only with css, adding animation
    of pressing to button while it is :active.
    But this didn't seem to work with button type=submit, so I came up
    with adding class "clicked" each time user clicks button and
    removing it after 0.5s when animation is over.

--- src/components/Buttons/ButtonBasic/index.tsx
+++ src/components/Buttons/ButtonBasic/index.tsx
@@ -1,3 +1,6 @@
+// hooks
+import { useEffect, useState } from "react";
+
 // local imports
 import classNames from "classnames";
 import style from "./style.module.scss";
@@ -5,17 +8,40 @@ import { ButtonBasicComponentI } from "./type";
 
 
 const ButtonBasic: React.FC<ButtonBasicComponentI> = (props) => {
+
+    // isClicked and useEffect are for adding click animation for mobile devices.
+    const [ isClicked, setIsClicked ] = useState<boolean>(false);
+    useEffect(() => {
+
+        if(!isClicked) return
+
+        const clickedTimer = setTimeout(() => {
+            setIsClicked(false);
+        }, 500);
+
+        return () => {clearTimeout(clickedTimer)}
+
+    })
+
+    const clickHandler = () => {
+        props.onClick();
+        setIsClicked(true);
+    }
+
+
     return (
         <button
             className={
                 classNames([
                     style.button,
+                    isClicked ? style.clicked : '',
                     style[props.theme],
                     props.positioning  ? props.positioning : '',
+                    "unselectable"
                 ])
             }
             type={props.type}
-            onClick={props.onClick}>
+            onClick={clickHandler}>
                 {props.text}
         </button>
     )
