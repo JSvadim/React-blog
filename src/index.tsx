3afdce949b1c29aac27d9d13f6b799d4b75aa43d
JSvadim <sadomazo323@gmail.com>
2023-01-03 20:35:34 +0300

    Changed .env for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 // third party
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
@@ -11,11 +11,11 @@ import { store } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
