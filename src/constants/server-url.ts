fc5846ad6511197d4a2e69ab5c454e86a8f3d921
JSvadim <sadomazo323@gmail.com>
2023-01-15 21:51:51 +0300

    changed serverURL variable's value

--- src/constants/server-url.ts
+++ src/constants/server-url.ts
@@ -1,2 +1,2 @@
-//  export const serverURL = 'https://react-blog-server-zstd.onrender.com'
- export const serverURL = 'http://localhost:5000'
+export const serverURL = 'https://react-blog-server-zstd.onrender.com'
+//  export const serverURL = 'http://localhost:5000'
