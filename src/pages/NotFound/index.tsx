bbf2b285bce9711403787021a8a51157416bdba1
JSvadim <sadomazo323@gmail.com>
2023-01-15 20:46:06 +0300

    Little fixes and additions
    After I added russian language content began to overflow "BasicButton"
    component. So I changed it's property "width" to "min-width".
    
    In styles of "User" component I increased "max-width" for
    ".nickname-container" since previous max-width wasn't enough for
    text to push wrapper making it bigger.
    
    I used styles from "LogIn" page's folder for "SignIn" page, I copied
    that file and placed into "SignIn" page's folder. I did this since
    height of wrapper div is different on both pages and might be in
    the future I'll need to add different styles for these pages,
    so it's better to have a styles file for every page.
    
    I added animation for title that appears on "Blogs" and "MyBlogs"
    pages when there are no blogs, same I made for title on "NotFound" page.

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,21 @@
+// hooks
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+
 // local imports
 import Container from "../../components/Container";
 import style from "./style.module.scss";
+import { notFoundPageVocabulary } from "../../vocabulary/pages/NotFound";
+
 
 const NotFoundPage: React.FC = () => {
+
+const { language } = useTypedSelector(state => state.language);
+
     return (
         <Container>
-            <div>PAGE NOT FOUND</div>
+            <h1 className={style["page-title"]}>
+                {notFoundPageVocabulary.title[language]}
+            </h1>
         </Container>
     )
 }
